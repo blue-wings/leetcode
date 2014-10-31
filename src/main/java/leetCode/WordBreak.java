457b54f67f63d0e28727720b9977f4da7e668f37
blue-wings <blue2048@gmail.com>
2014-10-31 16:14:34 +0800

    some code

---
+++ src/main/java/leetCode/WordBreak.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+import java.util.Set;
+
+/**
+ * User: FR
+ * Time: 10/31/14 2:15 PM
+ * Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+ * For example, given
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ * Return true because "leetcode" can be segmented as "leet code".
+ */
+public class WordBreak {
+
+    public boolean wordBreak(String s, Set<String> dict) {
+
+    }
+
+}
