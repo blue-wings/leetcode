75fe029007398c57b4dfb737aebe4c83e5a4f8a7
blue-wings <blue2048@gmail.com>
2014-12-23 17:28:06 +0800

    fix uuid bug

--- src/main/java/tools/UUID.java
+++ src/main/java/tools/UUID.java
@@ -1,5 +1,6 @@
 package tools;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
@@ -8,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * User: FR
  * Time: 12/22/14 5:53 PM
- * 每秒40w的id生成能力
+ * 每秒270w的id生成能力
  */
 public class UUID {
 
@@ -17,9 +18,10 @@ public class UUID {
     private final static Integer WORK_ID_BITS=5;
     private final static Integer DATA_CENTER_BITS=5;
     private final static Integer MILLI_SECOND_BITS=41;
+    private final static Integer MAX_SEQUENCE=-1 ^ (-1<<SEQUENCE_BITS);
     private final static Integer MAX_WORK_ID=-1 ^ (-1<<WORK_ID_BITS);
     private final static Integer MAX_DATA_CENTER_ID=-1 ^ (-1 << DATA_CENTER_BITS);
-    private final static Integer MAX_MILLISECONDS = -1 ^ (-1 << MILLI_SECOND_BITS);
+    private final static Long MAX_MILLISECONDS = -1L ^ (-1L << MILLI_SECOND_BITS);
 
     private final static Integer MILLI_SHIFT= DATA_CENTER_BITS + WORK_ID_BITS + SEQUENCE_BITS;
     private final static Integer DATA_CENTER_SHIFT= WORK_ID_BITS + SEQUENCE_BITS;
@@ -47,7 +49,7 @@ public class UUID {
     public synchronized long nextId(){
         long milliseconds = System.currentTimeMillis();
         if(this.lastTimestamp == milliseconds){
-            this.sequence = (this.sequence+1) & MAX_MILLISECONDS;
+            this.sequence = (this.sequence+1) & MAX_SEQUENCE;
             if(this.sequence == 0){
                 milliseconds = tilNextMillis(this.lastTimestamp);
             }
@@ -57,6 +59,9 @@ public class UUID {
             this.sequence = 0;
         }
         this.lastTimestamp = milliseconds;
+        if(((milliseconds-HORIZON_MILLI) & MAX_MILLISECONDS) == 0){
+            throw new RuntimeException("time is too large");
+        }
         long nextId = ((milliseconds-HORIZON_MILLI)<<MILLI_SHIFT)+(this.data_center_id<<DATA_CENTER_SHIFT)+(this.work_id<<WORK_ID_SHIFT)+this.sequence;
         return nextId;
     }
@@ -74,12 +79,12 @@ public class UUID {
         final ConcurrentHashMap set = new ConcurrentHashMap();
         final UUID uuid = new UUID(31, 1);
         final CountDownLatch countDownLatch = new CountDownLatch(100);
-        long start = System.currentTimeMillis();
+        final long start = System.currentTimeMillis();
         for (int i=0; i<100; i++){
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    while (System.currentTimeMillis() < 1419301841759L){
+                    while ((System.currentTimeMillis() - start)<6000){
                         long id = uuid.nextId();
                         set.put(id, "");
                         counter.incrementAndGet();
@@ -93,6 +98,17 @@ public class UUID {
         System.out.println("use time " + (end - start));
         System.out.println(counter.get() + "");
         System.out.println(set.keySet().size());
-        System.out.println(System.currentTimeMillis() + "");
+    }
+
+    public static void main1(String[] args){
+        System.out.println(Long.toBinaryString(MAX_MILLISECONDS));
+        long start = System.currentTimeMillis();
+        UUID uuid = new UUID(31, 1);
+        Set<Long> set = new HashSet<Long>();
+        while ((System.currentTimeMillis() - start)<6000){
+            long id = uuid.nextId();
+            set.add(id);
+        }
+        System.out.println(set.size());
     }
 }
